ecddb73f7a33d0828d582eaab920199fa99a4408
AOelschlaegel <AOelschlaegel@users.noreply.github.com>
2018-12-14 23:39:08 +0100

    Added Food to UI

--- Assets/ResourceManager.cs
+++ Assets/ResourceManager.cs
@@ -1,5 +1,18 @@
 using UnityEngine;
+using UnityEngine.UI;
+
 public class ResourceManager : MonoBehaviour
 { 
 	public int food;
+
+	public Text foodText;
+
+	private void Update()
+	{
+		if (food != 0)
+		{
+			foodText.text = "FOOD: " + food.ToString();
+		}
+		else foodText.text = null;
+	}
 }
