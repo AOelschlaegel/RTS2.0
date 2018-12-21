19ecbd4403a3d626179a3dbbf73b32d90b213f73
AOelschlaegel_MacBook <albertoelschlagel@alberts-MacBook-Pro.local>
2018-12-21 21:17:12 +0100

    Worked on TownCenterBehaviour

--- Assets/Scripts/Buildings/TownCenterBehaviour.cs
+++ Assets/Scripts/Buildings/TownCenterBehaviour.cs
@@ -1,99 +1,107 @@
 using UnityEngine;
 using UnityEngine.AI;
+using System.Collections.Generic;
 
 public class TownCenterBehaviour : MonoBehaviour
 {
-	#region Initialization
-	private SelectionManager _selectionManager;
-	[SerializeField] private GameObject _wayPointPrefab;
-	private ResourceManager _resourceManager;
-	[SerializeField] private GameObject _citizen;
+    #region Initialization
+    private SelectionManager _selectionManager;
+    [SerializeField] private GameObject _wayPointPrefab;
+    private ResourceManager _resourceManager;
+    [SerializeField] private GameObject _citizen;
 
-	private GameObject _wayPoint;
-	private GameObject _hitGameobject;
-	public GameObject SelectedObject;
+    private GameObject _wayPoint;
+    private GameObject _hitGameobject;
+    public GameObject SelectedObject;
 
-	public bool Selected;
-	public bool HasWaypoint;
+    [SerializeField] private float _citizenSpawnDuration;
+    [SerializeField] float _citizenSpawnTimer;
+
+    public Queue<string> BuildQueue;
+    private float currentBuildProgress = 0.0f;
+    public float maxBuildProgress;
+
+    public bool Selected;
+    public bool HasWaypoint;
 
     private Vector3 _hitPos;
 
-	#endregion
+    #endregion
 
-	#region UnityEvents
+    #region UnityEvents
 
-	private void Start()
-	{
-		HasWaypoint = false;
-		Selected = false;
+    private void Start()
+    {
+        HasWaypoint = false;
+        Selected = false;
 
-		_selectionManager = GameObject.Find("GameManager").GetComponent<SelectionManager>();
-		_resourceManager = GameObject.Find("ResourceManager").GetComponent<ResourceManager>();
-	}
+        _selectionManager = GameObject.Find("GameManager").GetComponent<SelectionManager>();
+        _resourceManager = GameObject.Find("ResourceManager").GetComponent<ResourceManager>();
+    }
 
-	private void Update()
-	{
-		Checks();
-		Inputs();
+    private void Update()
+    {
+        Checks();
+        Inputs();
 
-	}
+    }
 
-	#endregion
+    #endregion
 
-	#region CustomEvents
+    #region CustomEvents
 
-	private void Checks()
-	{
-		// Check if List is empty
-		if (_selectionManager.SelectedObjects.Count != 0)
-		{
-			SelectedObject = _selectionManager.SelectedObjects[0].gameObject;
+    private void Checks()
+    {
+        // Check if List is empty
+        if (_selectionManager.SelectedObjects.Count != 0)
+        {
+            SelectedObject = _selectionManager.SelectedObjects[0].gameObject;
 
-			//Check if this gameobject is selected
-			if (SelectedObject == gameObject)
-			{
-				Selected = true;
-			}
-			else Selected = false;
-		}
-		else
-		{
-			SelectedObject = null;
-			Selected = false;
-		}
+            //Check if this gameobject is selected
+            if (SelectedObject == gameObject)
+            {
+                Selected = true;
+            }
+            else Selected = false;
+        }
+        else
+        {
+            SelectedObject = null;
+            Selected = false;
+        }
 
-		// Check if waypoint exists
-		if (this.transform.childCount > 0)
-		{
-			HasWaypoint = true;
-		}
+        // Check if waypoint exists
+        if (this.transform.childCount > 0)
+        {
+            HasWaypoint = true;
+        }
 
-		//Check if Selected and already Has Waypoint
-		if (HasWaypoint)
-		{
-			if(Selected == true)
-			{
-				_wayPoint.GetComponent<Renderer>().enabled = true;
-			}
-			else _wayPoint.GetComponent<Renderer>().enabled = false;
-		}
-	}
+        //Check if Selected and already Has Waypoint
+        if (HasWaypoint)
+        {
+            if (Selected == true)
+            {
+                _wayPoint.GetComponent<Renderer>().enabled = true;
+            }
+            else _wayPoint.GetComponent<Renderer>().enabled = false;
+        }
+    }
 
-	private void Inputs()
-	{
-		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-		RaycastHit hit;
+    private void Inputs()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
 
-		if (Selected == true)
-		{
-			if (_resourceManager.food >= 30)
-			{
-				if(Input.GetKeyDown(KeyCode.F))
-				{
-					CreateCitizen();
-					_resourceManager.food -= 30;
-				}
-			}
+        if (Selected == true)
+        {
+            if (_resourceManager.food >= 30)
+            {
+                if (Input.GetKeyDown(KeyCode.F))
+                {
+                    CreateCitizen();
+                    _resourceManager.food -= 30;
+                }
+            }
             //Start Raycast
             if (Physics.Raycast(ray, out hit))
             {
@@ -103,66 +111,83 @@ public class TownCenterBehaviour : MonoBehaviour
 
                 if (Input.GetMouseButtonDown(1))
                 {
-                        // Check 
+                    // Check 
 
-                        switch (_hitGameobject.layer)
-                        {
-                            // Buildings
-                            case 10:
-                                _hitPos = _hitGameobject.transform.position;
-                                CreateWaypoint(_hitPos);
-                                break;
+                    switch (_hitGameobject.layer)
+                    {
+                        // Buildings
+                        case 10:
+                            _hitPos = _hitGameobject.transform.position;
+                            CreateWaypoint(_hitPos);
+                            break;
 
-                            // Ground
-                            case 11:
-                                _hitPos = hit.point;
-                                CreateWaypoint(_hitPos);
-                                break;
+                        // Ground
+                        case 11:
+                            _hitPos = hit.point;
+                            CreateWaypoint(_hitPos);
+                            break;
 
-                            //Units
-                            case 12:
+                        //Units
+                        case 12:
 
-                                break;
+                            break;
 
                         case 13:
                             _hitPos = _hitGameobject.transform.position;
                             CreateWaypoint(_hitPos);
 
                             break;
-                                }
-                                
-                                }
+                    }
 
-			}
-		}
-	}
+                 }
 
-	private void CreateWaypoint(Vector3 position)
-	{
-        if(HasWaypoint)
+            }
+        }
+    }
+
+    private void CreateWaypoint(Vector3 position)
+    {
+        if (HasWaypoint)
         {
             var waypoint = this.transform.GetChild(0).gameObject;
             Destroy(waypoint);
         }
 
         _wayPoint = Instantiate(_wayPointPrefab, position, Quaternion.identity);
-		_wayPoint.transform.parent = this.transform;
-	}
+        _wayPoint.transform.parent = this.transform;
+    }
 
-	private void CreateCitizen()
-	{
-        if (HasWaypoint)
-        {
-            var waypoint = this.transform.GetChild(0).gameObject;
-            var citizen = Instantiate(_citizen, transform.position, Quaternion.identity);
-            var agent = citizen.GetComponent<NavMeshAgent>();
-            agent.SetDestination(waypoint.transform.position);
-        }
-        else
-        {
-            Instantiate(_citizen, transform.position, Quaternion.identity);
-        }
-	}
+    private void CreateCitizen()
+    { 
+            if (HasWaypoint)
+            {
 
-	#endregion
+                var waypoint = this.transform.GetChild(0).gameObject;
+                var citizen = Instantiate(_citizen, transform.position, Quaternion.identity);
+                var agent = citizen.GetComponent<NavMeshAgent>();
+                agent.SetDestination(waypoint.transform.position);
+                _citizenSpawnTimer = _citizenSpawnDuration;
+            }
+            else
+            {
+                Instantiate(_citizen, transform.position, Quaternion.identity);
+                _citizenSpawnTimer = _citizenSpawnDuration;
+            }
+        }
+
+    protected void ProcessBuildQueue()
+    {
+        if (BuildQueue.Count > 0)
+        {
+            currentBuildProgress += Time.deltaTime;
+            if (currentBuildProgress > maxBuildProgress)
+            {
+                if (player) player.AddUnit(BuildQueue.Dequeue(), spawnPoint, transform.rotation);
+                currentBuildProgress = 0.0f;
+            }
+        }
+    }
 }
+
+    #endregion
+
